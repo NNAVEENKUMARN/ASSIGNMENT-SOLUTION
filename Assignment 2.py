189db5c3c4b0b7c4f65e6f7e67ac24d3b4e411a2
NNAVEENKUMARN <98763140+NNAVEENKUMARN@users.noreply.github.com>
2022-02-01 20:17:09 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,10 @@
+#GETTING MULTIPLE INPUTS AND THIER SUM
+print("**************")
+s=0
+x=int(input("Enter the total numbers to be summed:"))
+for i in range(x):
+    y=int(input("Enter the values :"))
+    s=s+y
+print(s)
+print("*************")
+        
